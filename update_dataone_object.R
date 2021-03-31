bd2f20af47258054e97ae474af8a241774ac2221
Graeme Diack <graemediack@hotmail.com>
2021-03-31 15:52:40 +0000

    improve search and update

--- update_dataone_object.R
+++ update_dataone_object.R
@@ -1,17 +1,27 @@
 # Search for and update an existing DataPackage
 # Find search terms - useful documents and examples
-vignette("searching-dataone")
+vignette(package="dataone")
+vignette("v03-searching-dataone")
 ??getQueryEngineDescription
-getQueryEngineDescription(mn,"solr")
+getQueryEngineDescription(cn,"solr")
 
-# Search
-queryParamList <- list(q="author:Graeme Diack", rows="100",fl="id,title,author,archived,obsoletedBy") # rows value just to override default of 10
-queryParamList <- list(q="keywords:WKSALMON", rows="100",fl="id,title,author,archived,obsoletedBy,dateUploaded")
-
+# Search - Some ideas that might or might not work :/
+queryParamList <- list(q="author:Graeme Diack", rows="10",fl="id,title,author,archived,obsoletedBy") # rows value just to override default of 10
+queryParamList <- list(q="keywords:'Likely Suspects Framework'", rows="100",fl="id,title,author,archived,obsoletedBy,dateUploaded")
 queryParamList <- list(q="id:*2d060720-25c0-4d6d-b37d-6edd7835d425", rows="1",fl="title,author,contactOrganization")
 
+# The actual working search to find all data objects with a specific rightsHolder (Heavily escaped ORCID)
+# ESCAPE-ORAMA! All these mad backslashes are just to escape solr special characters https://lucene.apache.org/core/6_5_1/queryparser/org/apache/lucene/queryparser/classic/package-summary.html#Escaping_Special_Characters
+# Not sure why I need two of each, just works
+queryParamList <- list(q="rightsHolder:http\\:\\/\\/orcid.org\\/0000\\-0003\\-1023\\-4700", rows="1000",fl="id,title,archived,obsoletedBy")
+
 result <- query(mn,solrQuery=queryParamList,as="data.frame")
-resultNotObselete <- filter(result,is.na(obsoletedBy))
+
+# remove obsoleted entries
+result <- filter(result,is.na(obsoletedBy))
+# remove those with no title (TODO: WHAT Are they?)
+result <- filter(result,!is.na(title))
+
 
 ############
 # Sometimes archive status can lag in the search results
@@ -31,13 +41,13 @@ accessRules <- data.frame(
   subject=c("CN=Likely Suspects Framework Users,DC=dataone,DC=org"),
   permission=c("read","write"))
 
-for(pid in resultNotObselete$id){
+for(pid in result$id){
   sysmeta <- getSystemMetadata(mn, pid)
   sysmeta <- addAccessRule(sysmeta, accessRules)
   #sysmeta <- removeAccessRule(sysmeta, accessRules)
   status <- updateSystemMetadata(mn, pid, sysmeta)
-  #print(pid)
-  #print(sysmeta@accessPolicy)
+  # print(pid)
+  # print(sysmeta@accessPolicy)
 }
 
 
